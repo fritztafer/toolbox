91b5f330f9488c076e200f7c7679091075ddd1d9
fritztafer <fritzistheman@gmail.com>
2025-05-14 17:05:15 -0700

    Removed global var from global.js and modified index.js to accomodate

--- js/global.js
+++ js/global.js
@@ -10,8 +10,7 @@ function run() {
 	let toolDiv = Object.assign(document.createElement("div"),{id:shortname});
 	let toolRun = Object.assign(document.createElement("script"),{innerHTML:shortname + "();"});
 	let banner = Object.assign(document.createElement("div"),{id:"banner"});
-
-	tools = // used by index.html. Add new tools here, eventually use php to pull names of .html files
+	let tools = // Add new tools here, eventually use php and fetch names of .html files
 		'<a href="/index.html"><img class="bannerImg" src="/img/index.png" title="index"></a>' +
 		'<a href="/tool/notepad.html"><img class="bannerImg" src="/img/notepad.png" title="notepad"></a>' +
 		'<a href="/tool/calculator.html"><img class="bannerImg" src="/img/calculator.png" title="calculator"></a>' +
--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 // index
 
 function index() {
-	let toolArray = tools.split("</a>");
-	console.log(toolArray);
-	for (i in toolArray) {
-		if (i < toolArray.length - 1 && i != 0) { // dont include index or dark mode button
-			let tool = toolArray[i].substring(toolArray[i].indexOf("tool/") + 5, toolArray[i].indexOf("."));
-			document.getElementById("index").innerHTML += "<a href=\"/tool/" + tool + ".html\"><p>" + tool + "</p></a>";
+	let toolArray = Array.from(document.getElementById("banner").children);
+	for (let i = 0; i < toolArray.length; i++) {
+		if (i < toolArray.length - 1 && i !== 0) { // skip index and dark mode button
+			let href = toolArray[i].getAttribute("href");
+			if (href && href.includes("tool/")) {
+				let tool = href.substring(href.indexOf("tool/") + 5, href.indexOf("."));
+				document.getElementById("index").innerHTML += `<a href="/tool/${tool}.html"><p>${tool}</p></a>`;
+			}
 		}
 	}
 }
